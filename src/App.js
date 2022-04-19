f34d3803af47bb257808abd905024e35b13c5df3
MagdaPaziewicz <95215239+MagdaPaziewicz@users.noreply.github.com>
2022-04-19 12:21:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import Header from './components/header/Header';
 import Form from './components/form/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+<>
+<Header />
+<Form />
+</>
+);
 
 export default App;
+
